585b2eaf59be605105ce203a136daa05b95e42cb
nag <nag@123>
2023-11-15 12:35:36 +0530

    first

--- 17.py
+++ 17.py
@@ -12,5 +12,5 @@ def process_file(f1):
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python script.py <file>")
-        sys.exit(1)add 17
+        sys.exit(1)
     process_file(sys.argv[1])
